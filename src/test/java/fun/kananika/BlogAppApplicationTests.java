74cc3d07d9990ddbbdaa086436221faa3d8205ec
rabiroshan shubhankar <rshubhankar11@gmail.com>
2022-04-04 17:34:57 +0530

    added db setting in properties and added packages

---
+++ src/test/java/fun/kananika/BlogAppApplicationTests.java
@@ -0,0 +1,13 @@
+package fun.kananika;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
